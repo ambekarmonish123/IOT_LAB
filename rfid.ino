dc236748ae6f3a8f5966a3fb2eae73c71ae1c86d
ambekarmonish123 <71320647+ambekarmonish123@users.noreply.github.com>
2021-12-11 12:54:47 +0530

    Create rfid.ino

---
+++ rfid.ino
@@ -0,0 +1,56 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(9,10);
+#define LEDPIN 12
+char tag[]="";
+int count=0;
+char input[12];
+boolean flag=0;
+
+void setup(){
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  pinMode(LEDPIN,OUTPUT);
+}
+
+void loop (){
+  if(mySerial.available())
+  {
+    count=0;
+    while(mySerial.available() && count <12)
+    {
+      input[count]=mySerial.read();
+      count++;
+      delay(5);
+    }
+    //Serial.print(input);
+
+   if(count==12)
+   {
+    count=0;
+    flag=1;
+    while(count<12 && flag!=0)
+    {
+      if(input[count]==tag[count])
+      flag=1;
+      else
+      flag=0;
+      count++;
+    }
+   }
+   if(flag==1){
+    Serial.println("Accesss Allowed");
+    digitalWrite(LEDPIN,HIGH);
+    delay(2000);
+    digitalWrite(LEDPIN,LOW);
+   }
+   else{
+    Serial.println("Accesss DENIED");
+    digitalWrite(LEDPIN,LOW);
+    delay(2000);
+   }
+   for(count=0;count<12;count++){
+    input[count]='F';
+   }
+   count=0;
+  }
+}
